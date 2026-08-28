e583bf5c757abd6672d73aa08dd76cb3e31434c2
Abdul-Rahman <armahmood786@yahoo.com>
2026-08-28 05:57:46 +0500

    Add files via upload

---
+++ week01/02-DebugDemo/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+    int x = 10;
+    int y = 20;
+    int result = x + y;
+    printf("Result = %d\n", result);
+    return 0;
+}
---
+++ week01/04-Test/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+    int age = 50;
+    printf("C compiler is working!\n");
+    printf("My age is %d.\n", age);
+    return 0;
+}
